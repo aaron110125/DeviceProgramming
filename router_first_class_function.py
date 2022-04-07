a114b67417df9180ba33fcbfb5cefb119c65a409
Aaron D'Costa <dcostaaaron@yahoo.in>
2022-04-07 00:09:16 -0700

    Update router_first_class_function.py

--- router_first_class_function.py
+++ router_first_class_function.py
@@ -8,5 +8,13 @@ def group_of_devices(protocol):
     router = protocol(""" Arista Router 7280CR3-96 is a feature of 7280R3 Series """)
     print(router)
 
+def create_adder(x):
+    def akamai_bar_raiser(y):
+        return x**y
+    return akamai_bar_raiser
+
+migeot = create_adder(3)
+print(migeot(3))
+
 group_of_devices(device1)
 group_of_devices(device2)

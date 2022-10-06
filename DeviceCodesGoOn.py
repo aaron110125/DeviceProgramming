737d0dfc9b2783b57b082436e46e649e273c5f01
Aaron D'Costa <dcostaaaron@yahoo.in>
2022-10-06 00:20:58 -0700

    Update DeviceCodesGoOn.py

--- DeviceCodesGoOn.py
+++ DeviceCodesGoOn.py
@@ -13,3 +13,30 @@ print("Router2 belongs to the family of {}".format(NetDevice2.__class__.device1)
 
 print("L2 routing will be done by {}".format(NetDevice.device_name))
 print("L3 routing will be done by {}".format(NetDevice2.device_name))
+
+## Second class to find out capacity in the network
+
+class addition_traffic_load:
+    first_router_load = 0
+    second_router_load = 0
+    third_router_load = 0
+
+    def __init__(self,first,second):
+        self.first = first
+        self.second = second
+
+    def display(self):
+        print("Traffic displayed on first_router is {} Gbps".format(str(self.first)))
+        print("Traffic displayed on first_router is {} Gbps".format(str(self.second)))
+        print("Total traffic in the network is {} Gbps".format(str(self.total_capacity)))
+
+    def calculate_total_capacity(self):
+        self.total_capacity = self.first + self.second
+
+cap = addition_traffic_load(12800,25600)
+cap.calculate_total_capacity()
+cap.display()
+
+
+
+
